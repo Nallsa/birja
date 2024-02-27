36e8639811bff44e951d691074cddf52dc8cd284
Aslan <abubakarovdev@gmail.com>
2024-02-27 15:40:27 +0300

    micro fix

---
+++ src/app/Support/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ButtonAdd from '@/svg/ButtonAdd';
+
+export default function Support() {
+  return (
+    <div>
+      <div className='flex justify-between w-full '>
+        <h1>Поддержка</h1>
+        <ButtonAdd />
+      </div>
+    </div>
+  );
+}
---
+++ src/svg/ButtonAdd.js
@@ -0,0 +1,19 @@
+export default function ButtonAdd() {
+  return (
+    <svg
+      width='40'
+      height='40'
+      viewBox='0 0 40 40'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect width='40' height='40' rx='6' fill='#D9D9D9' />
+      <path
+        d='M29 20H11M20 11V29'
+        stroke='white'
+        stroke-width='3'
+        stroke-linecap='square'
+      />
+    </svg>
+  );
+}
